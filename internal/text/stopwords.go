197608e4ed795d810a4d34a12dd52cdbe156a1b2
okelloodhiambocvs <okelloodhiambocvs@gmail.com>
2026-05-13 18:46:58 +0300

    Add stopword filtering for smarter ATS keyword analysis

---
+++ internal/text/stopwords.go
@@ -0,0 +1,19 @@
+package text
+
+var StopWords = map[string]bool{
+	"the": true,
+	"and": true,
+	"with": true,
+	"for": true,
+	"from": true,
+	"that": true,
+	"this": true,
+	"in": true,
+	"on": true,
+	"to": true,
+	"of": true,
+	"a": true,
+	"an": true,
+	"is": true,
+	"are": true,
+}
--- internal/text/tokenize.go
+++ internal/text/tokenize.go
@@ -2,6 +2,16 @@ package text
 
 import "strings"
 
-func Tokenize(text string) []string {
-	return strings.Fields(text)
+func Tokenize(input string) []string {
+	words := strings.Fields(input)
+
+	var cleaned []string
+
+	for _, w := range words {
+		if !StopWords[w] {
+			cleaned = append(cleaned, w)
+		}
+	}
+
+	return cleaned
 }
